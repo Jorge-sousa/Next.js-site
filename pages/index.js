0311421fa7037f6d24399b8a8464e9312c971fcf
Jorge de Sousa <63168978+Jorge-sousa@users.noreply.github.com>
2021-02-13 20:37:55 -0300

    adicionado página raças

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import Link from 'next/link';
 function Home(){
-    return <div>Hello, world!</div>
+    return (<div>
+        <h1>RPG</h1>
+        <Link href="/racas">
+        <a>Raças</a>
+        </Link>
+    </div>)
 }
 export default Home
---
+++ pages/racas.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+function Raca(){
+    return (<div>
+        <h1>Raças</h1>
+        <Link href="/">
+        <a>Retornar para página inicial.</a>
+        </Link>
+    </div>)
+}
+export default Raca
